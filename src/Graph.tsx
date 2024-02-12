5d44acb42227c0c0c448a3f5151d0116efa0d097
isaac <olivareshernandezisaac@gmail.com>
2024-02-12 15:52:39 -0600

    modified both graph.tsx and datamanipulator.ts in order for the graph to show ther ratios between both stocks and slow to display the upper and lower bounds to alert when the bounds corss'

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import { Table, TableData } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -37,24 +37,29 @@ class Graph extends Component<IProps, {}> {
     }
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
+      //removed ('column-pivots', '["stock"]') since we now want the ratio of both stocks and not each individual stock
+      //'columns', '["top_ask_price"]'; removed since we are now dealing with the ratio,lower bound, upper bound and the trigger alert on the y axis.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
       }));
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
+      ]as unknown as TableData //tells TypeScript to treat the array as type TableData
       );
     }
   }
